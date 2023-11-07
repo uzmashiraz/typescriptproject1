d8e9c355eeded5580e0489675ae20ff9dc58c4c9
uzmashiraz <145215173+uzmashiraz@users.noreply.github.com>
2023-11-07 22:16:22 +0500

    Create main.ts

---
+++ 45Q/main.ts
@@ -0,0 +1,278 @@
+ASSIGNMENT 1 QUESTIONS 45
+
+QUESTION 1
+INSTALLED NODE.JS, TYPESCRIPT AND VS CODE 
+
+QUESTION 2
+let personName = "UZMA";
+console.log(`Hello ${personName}, would you like to learn some python today?`);
+
+//QUESTION 3
+let personName1 = "UZMA, ATOOFA, Vaneeza";
+console.log(personName1);
+
+//question 4
+NAME:UZMA, DATE:10/09/2023, FAMOUS QUOTE PROGRAM
+let famousQuote = "Albert Einstein once said,";
+let famousQuote1 = "A person who never made a mistake never tried anything new.";
+console.log(`${famousQuote} "${famousQuote1}"`);
+
+//QUESTION 5
+let famous_person = "Albert Einstein once said,";
+let message = "A single piece of paper can't decide my future.";
+let message1 = `${famous_person} "${message}"`;
+console.log(message1);
+
+QUESTION 6
+let person_name = "\tuzma\n,\tatoofa\n,\tvaneeza\n";
+console.log(person_name);
+
+QUESTION 7 AND QUESTION 8
+NAME :UZMA, DATE:10/09/2023, ARITHMETIC OPERATIONS PREOGRAM
+console.log("ADDITION");
+console.log(5+3);
+console.log("SUBTRACTION");
+console.log(10-2);
+console.log("MULTIPLICATION");
+console.log(4*2);
+console.log("DIVISION");
+console.log(16/2);
+
+QUESTION 9
+let fav_num = 1;
+let meassage2 = `My favorite number is ${fav_num}`;
+console.log(meassage2);
+
+QUESTION 10
+ADDING COMMENTS
+
+QUESTION 11
+
+let names = ["uzma","Atoofa","Amir","Vaneeza","Muhib"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+console.log(names[4]);
+
+OUESTION 12
+
+let greetings = ["uzma","Atoofa","Amir","Vaneeza","Muhib"];
+console.log(`Hi,${greetings[0]} you are so genius`);
+console.log(`Hi,${greetings[1]} you are so genius`);
+console.log(`Hi,${greetings[2]} you are so genius`);
+console.log(`Hi,${greetings[3]} you are so genius`);
+console.log(`Hi,${greetings[4]} you are so genius`);
+
+QUESTION 13
+
+let myFavCars = ["Audi","Mercedes","BMW","Fortunar","Prado","Range Rover"];
+console.log(`I would like to own a ${myFavCars[5]}`);
+
+QUESTION 14
+
+let dinnerInvite = ["Fizza","Iqra Shabbir","Fizza Nasir"];
+console.log(`I would like to invite you for dinner my best friend ${dinnerInvite[0]}`);
+console.log(`I would like to invite you for dinner my friend ${dinnerInvite[1]}`);
+console.log(`I would like to invite you for dinner my best friend ${dinnerInvite[2]}`);
+
+QUESTION 15
+
+let dinnerInvite = ["Fizza","Iqra Shabbir","Fizza Nasir"];
+console.log(`I can't make it this meesage is said to ${dinnerInvite[0]}`);
+dinnerInvite.shift();
+dinnerInvite.unshift("Ujala");
+console.log(dinnerInvite);
+console.log(`Yes i am in ${dinnerInvite[0]},${dinnerInvite[1]},${dinnerInvite[2]}`);
+
+QUESTION 16
+
+let dinnerInvite = ["Fizza","Iqra Shabbir","Fizza Nasir"];
+console.log(`Hey, guys I found a bigger dinner table, so now more space is available`);
+dinnerInvite.unshift("Ujala");
+dinnerInvite.splice(2,0,"Kainat");
+dinnerInvite.push("Atoofa");
+console.log(dinnerInvite);
+console.log(`Hey, My friend ${dinnerInvite[0]} I want to invite you for a dinner at my house`);
+console.log(`Hey, My friend ${dinnerInvite[1]} I want to invite you for a dinner at my house`);
+console.log(`Hey, My friend ${dinnerInvite[2]} I want to invite you for a dinner at my house`);
+console.log(`Hey, My friend ${dinnerInvite[3]} I want to invite you for a dinner at my house`);
+console.log(`Hey, My friend ${dinnerInvite[4]} I want to invite you for a dinner at my house`);
+console.log(`Hey, My friend ${dinnerInvite[5]} I want to invite you for a dinner at my house`);
+
+QUESTION 17
+
+let dinnerInvite = ["Fizza","Iqra Shabbir","Fizza Nasir"];
+console.log(`Sorry,guys my new dinner table did not arrive my home so i'll have only two people space my dinner table`);
+dinnerInvite.pop();
+console.log(dinnerInvite);
+console.log(`Hey, My friend ${dinnerInvite[0]} you still invite a dinner`);
+console.log(`Hey, My friend ${dinnerInvite[1]} you still invite a dinner`);
+dinnerInvite.pop();
+dinnerInvite.pop();
+console.log(dinnerInvite);
+
+
+QUESTION 18
+
+let favPlaces = ["Maldives","Abu Dhabi","America","Australia","Canada","London"];
+console.log(favPlaces);
+console.log(favPlaces.reverse());
+console.log(favPlaces);
+console.log(favPlaces.sort());
+console.log(favPlaces);
+
+QUESTION 19
+
+let dinnerGuest = ["Fizza","Iqra Shabbir","Fizza Nasir"];
+console.log(`Hey, my friend ${dinnerGuest[0]} you are invited on dinner`);
+console.log(`Hey, my friend ${dinnerGuest[1]} you are invited on dinner`);
+console.log(`Hey, my friend ${dinnerGuest[2]} you are invited on dinner`);
+
+
+QUESTION 20
+
+let favThings = [];
+
+favThings.push("Countries","Cities","Mountains","Rivers","Foods");
+console.log(favThings);
+
+QUESTION 21
+
+let typescriptobjects = {
+    name: "uzma",
+    age: 20,
+    address: "Karachi"
+
+};
+console.log(typescriptobjects);
+
+QUESTION 22
+
+let intentionalError = ["uzma","Atoofa","Vaneeza","Amir","Muhib"]
+Contain Error in Index Program
+console.log(intentionalError.[5]);
+Correct Program
+console.log(intentionalError[0]);
+
+QUEATION 23
+
+let car = "subaru";
+console.log("Is car == subaru? I predict True")
+console.log(car == "subaru");
+let food = "Biryani";
+console.log("Is food == Biryani? I predict True")
+console.log(food == "Biryani");
+let item = "Shoes";
+console.log("Is item == Shoes? I predict False")
+console.log(item == "Watch");
+let car1 = "Audi";
+console.log("Is car1 == Audi? I predict False")
+console.log(car1 == "BMW");
+
+QUEATION 24
+
+let food = "Burger";
+let food1 = "pizza";
+let foodOrders = 5;
+console.log(food == "Burger");
+console.log(food1 == "biryani");
+console.log(foodOrders >= 5);
+console.log(foodOrders < 4);
+console.log(foodOrders < 3 && foodOrders > 5);
+console.log(foodOrders >= 5 || foodOrders < 15);
+
+
+QUESTION 25
+
+ALIEN COLORS #1
+
+let alien_color = "Green";
+if(alien_color === "Green"){
+    console.log("The player just earned 5 points");
+} 
+else{
+    console.log("The player just earned 10 points");
+}
+
+
+Question 26
+
+ALIEN COLORS #2
+
+let alien_color2 = "Green";
+if(alien_color2 === "Green"){
+    console.log("The player just earned 5 points for shooting the alien");
+} 
+else if (alien_color2 === "Yellow"){
+    console.log("The player just earned 10 points for shooting the alien");
+}
+else {
+    console.log("Game End");
+}
+
+
+Question 27
+
+Alien Colors #3
+
+let alien_color3 = "Green";
+
+if(alien_color3 === "Green"){
+    console.log("The player just earned 5 points for shooting the alien");
+} 
+else if (alien_color3 === "Yellow"){
+    console.log("The player just earned 10 points for shooting the alien");
+}
+else if (alien_color3 === "Red"){
+    console.log("The player just earned 15 points for shooting the alien")
+}
+else {
+    console.log("Game End");
+}
+
+Question 28
+
+let age = 3;
+
+if(age < 2){
+    console.log("The person is a baby");
+}
+else if (age < 4){
+    console.log("The person is a toddler");
+}
+else if (age < 13){
+    console.log("The person is a kid");
+}
+else if (age < 20){
+    console.log("The person is a teenager");
+}
+else if (age < 65){
+    console.log("The person is an Adult");
+}
+else if (age >= 65){
+    console.log("The person is a elder");
+}
+else{
+    console.log("Wrong Age");
+}
+
+
+Question 29
+
+let favFruit = ["Orange","Cherry","Mango"];
+let fruit = favFruit[0];
+let fruit1 = favFruit[1];
+let fruit3 = favFruit[2]
+if (fruit === "Apple"){
+    console.log("You really like Oranges")
+}
+else if (fruit1 === "Grapes"){
+    console.log("You really like Cherry");
+}
+else if (fruit3 === "Mango"){
+    console.log("You really like Mango");
+}
+else{
+    console.log("Try Again")
+}
